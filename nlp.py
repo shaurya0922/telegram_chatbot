b191d5dc9f9dc1f99f06806fb1a5c4aedd26c339
Shaurya Katiyar <shaurya.katiyar@olamnet.com>
2025-09-24 14:31:04 +0530

    Removed tracking other files

--- nlp.py
+++
@@ -1,8 +0,0 @@
-import nltk
-nltk.download('punkt')
-from nltk.tokenize import word_tokenize
-
-sentence = "I will be running for fifteen kilometers today!"
-tokens = word_tokenize(sentence)
-print(tokens)
-# ['Chatbots', 'are', 'becoming', 'smarter', 'every', 'day', '.']
